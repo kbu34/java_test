4386cfd33010d1a75d2c620884626beaff84d426
Phillip Kim <kbu34@uclive.ac.nz>
2022-07-10 15:43:56 +1200

    Set up input variables and validate them. Each variable gets its own setter. Within each setter, the input is validated. Exceptions are raised if the input is invalid.

--- TransactionApi.java
+++ TransactionApi.java
@@ -6,4 +6,40 @@ public class TransactionApi {
     private long cardNum;
     private int cvc;
     private Date expiryDate;
+
+    public void setName(String newName) {
+        try {
+            this.name = newName;
+            if (this.name.length() > 50) {
+                throw new Exception("Invalid name");
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void setCardNum(long newNum) {
+        try {
+            this.cardNum = newNum;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void setCvc(int newCvc) {
+        try {
+            this.cvc = newCvc;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void setExpiryDate(Date newExpiry) {
+        try {
+            this.expiryDate = newExpiry;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
 }
